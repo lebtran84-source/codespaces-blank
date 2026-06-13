a779bd953c21feda99656d0210ca757ea3ed9f88
lebtran84-source <lebtran84@gmail.com>
2026-06-13 21:52:49 +0700

    Update set.hpp

--- Library/set.hpp
+++ Library/set.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include<iostream>
 #include"avl.hpp"
+// Khoi tao 1 struct gom mot root co kieu du lieu la cay avl, va cap kich thuoc cho set
 template<typename T>
 struct myset{
     Node<T>* root;
     int sz;
 };
 template<typename T>
+// Khoi tao mot set rong
 myset<T>* initSet(){
     myset<T>* s = new myset<T>();
     s->root = NULL;
@@ -14,11 +16,13 @@ myset<T>* initSet(){
     return s;
 }
 template<typename T>
+// Kiem tra co ton tai val trong set chua
 bool contain(myset<T>* s, T val){
     if(!s)return false;
     return search(s->root,val);
 }
 template<typename T>
+// Neu set chua khoi tao thi khoi tao set. Neu khong thi tiep tuc kiem tra trong set co ton tai val khong, neu khong co moi them vao set
 void insertSet(myset<T>*& s, T val){
     if(!s){
         s = new myset<T>();
@@ -29,6 +33,7 @@ void insertSet(myset<T>*& s, T val){
     }
 }
 template<typename T>
+//  Giai phong vung nho cua set
 void clearSet(myset<T>* s){
     if(!s)return;
     clearTree(s->root);
