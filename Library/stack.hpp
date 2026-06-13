b6c0ee17e4cf005fe9dc2f105e191ecf4401f8be
lebtran84-source <lebtran84@gmail.com>
2026-06-13 23:08:11 +0700

    Update stack.hpp

--- Library/stack.hpp
+++ Library/stack.hpp
@@ -5,41 +5,61 @@ template <typename T>
 struct stack{
     T* data;
     int top, capacity;
-    // Khoi tao ngan xep voi cap phan tu toi da
-    stack(int cap){
-        capacity = cap;
-        data = new T[capacity];
-        top = -1;
-    }
-    // Giai phong ngan xep khi stack bi xoa
-    ~stack(){
-        delete[] data;
-    }
-    // Them mot phan tu vao cuoi 
-    void push(T val){
-        if(top== capacity -1){
-            cout<<"Stack is full\n";
-            return;
-        }
-        top++;
-        data[top] = val;
-    }
-    // Loai bo phan tu cuoi cung ra khoi ngan xep
-    T pop(){ 
-        if(top== -1){
-            cout<<"Stack is empty\n";
-            return T();
-        }
-        T val = data[top];
-        top--;
-        return val;
-    }
-    // Kiem tra xem ngan xep co trong khong
-    bool empty(){
-        return top == -1;
-    }
-    // Tra ve so phan tu hien co trong ngan xep
-    int size(){
-        return top+1;
-    }
 };
+
+template<typename T>
+// Khoi tao ngan xep voi cap phan tu toi da
+stack<T>* initStack(int cap){
+    stack<T>* st = new stack<T>();
+    st->capacity = cap;
+    st->data = new T[cap];
+    st->top = -1;
+    return st;
+}
+template<typename T>
+// Them mot phan tu vao cuoi 
+void push(stack<T>* st, T val){
+    if (!st) return;
+    if(st->top== st->capacity -1){
+        cout<<"Stack is full\n";
+        return;
+    }
+    st->top++;
+    st->data[st->top] = val;
+}
+template<typename T>
+// Loai bo phan tu cuoi cung ra khoi ngan xep
+T pop(stack<T>* st){ 
+    if(!st || st->top== -1){
+        cout<<"Stack is empty\n";
+        return T();
+    }
+    T val = st->data[st->top];
+    st->top--;
+    return val;
+}
+template<typename T>
+T top(stack<T>* st){
+    if(!st || st->tp == -1){
+        cout<<"Stack is empty\n";
+        return T();
+    } 
+    return st->data[st->top];
+}
+template<typename T>
+// Kiem tra xem ngan xep co trong khong
+bool empty(stack<T>* st){
+    return st->top == -1;
+}
+template<typename T>
+// Tra ve so phan tu hien co trong ngan xep
+int size(stack<T>* st){
+    return st->top+1;
+}
+template<typename T>
+// Giai phong ngan xep khi stack bi xoa
+void clearStack(stack<T>* st){
+    if(!st)return;
+    delete[] st->data;
+    delete st;
+}
